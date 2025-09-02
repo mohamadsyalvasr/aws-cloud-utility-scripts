ec7c4336b2b6eeb13c85cb9377b1359449ef71e1
mohamadsyalvasr <170172608+mohamadsyalvasr@users.noreply.github.com>
2025-09-02 12:27:39 +0700

    Update main_report_runner.sh

--- main_report_runner.sh
+++ main_report_runner.sh
@@ -14,14 +14,14 @@ log "🚀 Starting combined AWS report generation..."
 
 # Set execute permissions for all necessary scripts
 log "🔧 Setting execute permissions for all report scripts..."
-chmod +x ./aws_inventory_instance_report.sh
+chmod +x ./aws_inventory.sh
 chmod +x ./aws_sp_ri_report.sh
-chmod +x ./ebs_volume_report.sh
+chmod +x ./ebs_report.sh
 log "✅ Permissions set."
 
 # Check if the required scripts exist
-if [[ ! -f "./aws_inventory_instance_report.sh" || ! -f "./aws_sp_ri_report.sh" || ! -f "./ebs_volume_report.sh" ]]; then
-    log "❌ Error: One or more required scripts (aws_inventory_instance_report.sh, aws_sp_ri_report.sh, ebs_volume_report.sh) are missing."
+if [[ ! -f "./aws_inventory.sh" || ! -f "./aws_sp_ri_report.sh" || ! -f "./ebs_report.sh" ]]; then
+    log "❌ Error: One or more required scripts (aws_inventory.sh, aws_sp_ri_report.sh, ebs_report.sh) are missing."
     log "Please ensure all scripts are in the same directory."
     exit 1
 fi
@@ -29,7 +29,7 @@ fi
 # Pass all arguments to aws_inventory.sh and ebs_report.sh
 # but exclude -b and -e for aws_sp_ri_report.sh
 log "Running aws_inventory.sh..."
-./aws_inventory_instance_report.sh "$@"
+./aws_inventory.sh "$@"
 
 log "Running aws_sp_ri_report.sh..."
 # Filter out -b and -e arguments for the SP/RI script
@@ -53,7 +53,7 @@ done
 ./aws_sp_ri_report.sh "${FILTERED_ARGS[@]}"
 
 log "Running ebs_report.sh..."
-./ebs_volume_report.sh "$@"
+./ebs_report.sh "$@"
 
 log "✅ All reports generated successfully."
 log "Your reports are now available in the current directory."
